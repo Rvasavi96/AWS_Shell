a7a899abcf82eed00a970ac23fcbaed95e846ce6
Rvasavi96 <rapuru.vasavi246@gmail.com>
2024-08-24 19:55:34 +0530

    hi world

---
+++ 03-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash/
+###3rd one 
+Person1=$1 #this value is given from commandline as siva
+Person2=$2 #this value is given from commandline as charan
+echo "Hi $Person1 how are you"
+echo "Hi $Person2 im fine"
