cc078dc7c39b43a25c3e7407e303bd11e7f181d8
Rvasavi96 <rapuru.vasavi246@gmail.com>
2024-08-24 19:47:29 +0530

    hi world

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,3 +4,9 @@ Person1=Vasavi
 Person2=Rapuru
 echo "Hi $Person1 how are you"
 echo "Hi $Person2 im fine"
+
+
+##2nd one 
+##Shell script will execute the command inside the $() and stores the output in DATE Variable.
+DATE = $(date)
+ echo " this program is executed at the time and day: $(DATE)
