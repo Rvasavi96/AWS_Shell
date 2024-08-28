ad4c45de51878c127e256bcb4943f11126ce5022
Rvasavi96 <rapuru.vasavi246@gmail.com>
2024-08-28 12:09:40 +0530

    commitiong condiitons

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -4,6 +4,17 @@ userid=$(id -u)
 if [userid -ne 0]
 then 
 echo "Error: please run the script with root access"
+#else 
+#echo "your are the root user"
+fi
+# its our responsibility again to check the installaion is success or not 
+
+yum install mysql -y 
+
+if [ $? -ne 0 ]
+then 
+echo "installation of my sql is a error "
+exit 1
 else 
-echo "your are the root user"
+echo "installation of mysql is a success"
 fi
